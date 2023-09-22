12f1ac55adf70b182379471e6323178eda83196f
MiniWalls <69449726+MiniWalls@users.noreply.github.com>
2023-09-22 20:43:28 +0300

    Added sort button for posts, posts arent sorted yet

--- src/components/PostsGrid.tsx
+++ src/components/PostsGrid.tsx
@@ -1,4 +1,6 @@
-import PostItem from "../components/PostItem";
+import { useState, useEffect } from "react";
+import PostItem from "./PostItem";
+import PostSortListBox from "./PostSortListBox";
 
 type Post = {
   id: number;
@@ -13,9 +15,34 @@ interface ComponentProps {
     posts: Post[];
 }
 
+const sortOptions = [
+  {id: 1, name: "ID"},
+  {id: 2, name: "Title"},
+  {id: 3, name: "Username"},
+  {id: 4, name: "Date"},
+];
+
 const PostsGrid = (props: ComponentProps): JSX.Element => {
+  const [sort, setSort] = useState<string>(sortOptions[0].name);
+
+  useEffect(() => {
+    console.log(sort);
+  }, [sort]);
+
+  const changeSort = (id: string) => {
+    const option = sortOptions.find((item) => item.name === id);
+    if(option) {
+      setSort(option.name);
+    } else {
+      setSort(sortOptions[0].name);
+    }
+  };
+
   return(
     <div className="md:max-w-6xl  mx-auto justify-center items-center mb-10">
+      <div className="flex justify-end pb-2">
+        <PostSortListBox changeSort={changeSort} sortOptions={sortOptions}/>
+      </div>
       <div className="grid sm:grid-cols-1 md:grid-cols-3 gap-6 list-none">
         {props.posts.map((item: Post) => (
           <li key={item.id}>
